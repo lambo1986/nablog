fbed7cf191840812a2e811dba9cc50e9ec5aca60
Nathan Lambertson <lambertones@gmail.com>
2024-08-08 09:36:15 -0500

    Add some links to the home page

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -8,4 +8,28 @@ RSpec.describe "Root Page", type: :feature do
     expect(page).to have_content("Nathan's Junkyard")
     expect(page).to have_content("A collection of stuff I don't want to keep to myself, lest I forget about it.")
   end
+
+  it "has a link to the 'About' page" do
+    visit "/"
+
+    expect(page).to have_link("About Nathan")
+  end
+
+  it "has a link to the 'Contact' page" do
+    visit "/"
+
+    expect(page).to have_link("Contact Nathan")
+  end
+
+  it "has a link to the 'Junkyard' page" do
+    visit "/"
+
+    expect(page).to have_link("Junkyard")
+  end
+
+  it "has a link to the 'Thoughts' page" do
+    visit "/"
+
+    expect(page).to have_link("Thoughts")
+  end
 end

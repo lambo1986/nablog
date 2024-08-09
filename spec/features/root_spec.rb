a00404f66c4fc78bd164b074f0f4d9a0a6965889
Nathan Lambertson <lambertones@gmail.com>
2024-08-09 14:07:58 -0500

    Test for edge case with mailer

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -44,5 +44,16 @@ RSpec.describe "Root Page", type: :feature do
 
       expect(page).to have_content("Thank you for subscribing!")
     end
+
+    it "does not send an email if the form is incomplete" do
+      fill_in "Your Name", with: ""
+      fill_in "Your Email Address", with: "testuser@example.com"
+
+      expect {
+        click_button "Subscribe"
+      }.not_to change { ActionMailer::Base.deliveries.count }
+
+      expect(page).to have_content("There was an error with your subscription. Please try again.")
+    end
   end
 end

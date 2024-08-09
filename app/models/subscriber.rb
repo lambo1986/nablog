8b61eca41a0b0c48aaa7418b2b54f657c7398492
Nathan Lambertson <lambertones@gmail.com>
2024-08-08 20:31:13 -0500

    Fixthe Validationsback

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,9 @@
 class Subscriber < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_length_of(:email).is_at_most(255) }
+    it { should allow_value('example@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
+    it { should validate_presence_of(:name) }
+  end
 end

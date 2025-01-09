6617a51e78e274d57c9cd8ec3952ba40235e113c
lambo1986 <lambertones@gmail.com>
2025-01-09 14:41:52 -0500

    Begin new tests for mailer

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -1,5 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe SubscriberMailer, type: :mailer do
-  
+    describe 'welcome_email' do
+        let(:subscriber) { create(:subscriber, email: 'test@example.com') }
+        let(:mail) { SubscriberMailer.welcome_email(subscriber) }
+
+        it 'renders the headers' do
+            expect(mail.subject).to eq("Welcome to Nathan's Junkyard!")
+            expect(mail.to).to eq(['test@example.com'])
+            expect(mail.from).to eq(['default_from@example.com']) # Replace with your actual "from" email
+        end
+
+        it 'renders the body' do
+        expect(mail.body.encoded).to match("Welcome to Nathan's Junkyard")
+        end
+    end
 end

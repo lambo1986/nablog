178225366e7b693aab84e24d82ee8917663b5787
lambo1986 <lambertones@gmail.com>
2025-01-12 09:11:28 -0500

    Add art to the thoughts page, as well as a journal entry.

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,5 +1,5 @@
 class ThoughtsController < ApplicationController
   def index
-    
+    @thoughts_art1 = ArtFacade.new.art_single(47895)
   end
 end
--- spec/facades/art_facade_spec.rb
+++ spec/facades/art_facade_spec.rb
@@ -9,5 +9,13 @@ RSpec.describe ArtFacade, vcr: true, type: :facade do
       expect(art.title).to be_a(String)
       expect(art.image_url).to be_a(String)
     end
+
+    it "returns another single artwork" do
+      art = ArtFacade.new.art_single(47895)
+
+      expect(art).to be_a(ArtPoro)
+      expect(art.title).to be_a(String)
+      expect(art.image_url).to be_a(String)
+    end
   end
 end

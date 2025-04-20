9f724d5f2a535de7ae1aa4895f7ee92ae44c18d6
lambo1986 <lambertones@gmail.com>
2025-04-20 08:56:21 -0400

    Add another image to the thoughts index and a test for it

--- spec/features/thoughts/index_spec.rb
+++ spec/features/thoughts/index_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe "thoughts index page", type: :feature do
     expect(current_path).to eq("/thoughts")
     expect(page).to have_content("Thoughts by Nathan")
   end
+
+  it "has images from an API" do 
+    visit thoughts_path 
+
+    expect(current_path).to eq("/thoughts")
+    expect(page).to have_css("img[alt='Thought Art 1']")
+    expect(page).to have_css("img[alt='Thought Art 2']")
+  end
 end

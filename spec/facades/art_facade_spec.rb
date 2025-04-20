023b04651f160b504a37e6c751ddac1cdd44d9c0
lambo1986 <lambertones@gmail.com>
2025-04-20 07:32:35 -0400

    Add contact info to view with a test

--- spec/facades/art_facade_spec.rb
+++ spec/facades/art_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ArtFacade, vcr: true, type: :facade do
   describe "#art_single" do
     it "returns a single artwork" do
       art = ArtFacade.new.art_single(34545)
-
+      
       expect(art).to be_a(ArtPoro)
       expect(art.title).to be_a(String)
       expect(art.image_url).to be_a(String)
--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe "contact page", type: :feature do
       expect(page).to have_content("Contact Nathan Lambertson")
     end
   end
+
+  describe "GET /contact" do 
+    it "should have my phone number and email address" do 
+      visit contact_path
+
+      expect(page).to have_content("Phone Number:")
+      expect(page).to have_content("Email:")
+    end
+  end
 end
